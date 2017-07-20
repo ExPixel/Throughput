e6124190cef7de33cd531225703bbae74d566037
Adolph C <adolph.celestin@gmail.com>
2017-07-19 20:48:27 -0400

    Added the passthrough flag and reorganized code.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate clap;
 
 use clap::{App, Arg};
-use std::io::{stdin, stdout, Write, Read};
+use std::io::{stderr, stdin, stdout, Write, Read};
 use std::time::{Duration, Instant};
 use std::net::{SocketAddr, TcpListener, IpAddr};
 
@@ -9,17 +9,34 @@ const DEFAULT_BUFFER_SIZE: usize = 4096;
 const DEFAULT_ITERATION_COUNT: usize = 1;
 const DEFAULT_ADDRESS: &'static str = "127.0.0.1";
 
+macro_rules! print_err_into {
+    ($err_write: expr, $fmt:expr) => ({
+        use std::io::Write;
+        if let Err(e) = writeln!($err_write, $fmt) {
+            panic!("Error while writing to stderr: {}", e);
+        }
+    });
+
+    ($err_write: expr, $fmt:expr, $($arg:tt)*) => ({
+        use std::io::Write;
+        if let Err(e) = writeln!($err_write, $fmt, $($arg)*) {
+            panic!("Error while writing to stderr: {}", e);
+        }
+    });
+}
+
+
 macro_rules! print_err {
     ($fmt:expr) => ({
         use std::io::{stderr, Write};
-        if let Err(e) = write!(stderr(), $fmt) {
+        if let Err(e) = writeln!(stderr(), $fmt) {
             panic!("Error while writing to stderr: {}", e);
         }
     });
 
     ($fmt:expr, $($arg:tt)*) => ({
         use std::io::{stderr, Write};
-        if let Err(e) = write!(stderr(), $fmt, $($arg)*) {
+        if let Err(e) = writeln!(stderr(), $fmt, $($arg)*) {
             panic!("Error while writing to stderr: {}", e);
         }
     });
@@ -43,9 +60,14 @@ struct TransferInfo {
     last_bytes_transferred: usize,
 }
 
+#[inline]
+fn exit_err() -> ! {
+    std::process::exit(1);
+}
+
 fn main() {
     let matches = App::new("Throughput")
-        .version("1.0")
+        .version("1.1")
         .author("Adolph C.")
         .about("Measures the throughput of stdin or a socket.")
         .arg(Arg::with_name("address")
@@ -71,9 +93,14 @@ fn main() {
             .value_name("PORT_NUMBER")
             .help("Port to listen on. Must be specified if address is given.")
             .takes_value(true))
+        .arg(Arg::with_name("pass")
+            .long("pass")
+            .help("If present, throughput will print to stderr and pass input to stdout.")
+            .takes_value(false))
         .after_help("If a port/address is not specified, throughput will read from stdin.")
         .get_matches();
 
+    let passthrough = matches.is_present("pass");
     let buffer_size: usize;
     let iterations: usize;
 
@@ -82,7 +109,7 @@ fn main() {
             buffer_size = bsize;
         } else {
             print_err!("Buffer size must be a valid number.");
-            std::process::exit(1);
+            exit_err();
         }
     } else {
         buffer_size = DEFAULT_BUFFER_SIZE;
@@ -94,7 +121,7 @@ fn main() {
             iterations = it;
         } else {
             print_err!("Iterations must be a valid number.");
-            std::process::exit(1);
+            exit_err();
         }
     } else {
         iterations = DEFAULT_ITERATION_COUNT;
@@ -105,29 +132,29 @@ fn main() {
     if address_present || port_present {
         if !port_present {
             print_err!("A port must be speicified alongside a address.");
-            std::process::exit(1);
+            exit_err();
         } else {
             let address = matches.value_of("address").unwrap_or(DEFAULT_ADDRESS);
             let port = matches.value_of("port").expect("Expected port arg to have value.");
 
             if let Ok(parsed_port) = port.parse() {
-                measure_tcp_stream(address, parsed_port, buffer_size, iterations);
+                measure_tcp_stream(address, parsed_port, buffer_size, iterations, passthrough);
             } else {
                 print_err!("Port must be a valid number from 0 to 65535");
-                std::process::exit(1);
+                exit_err();
             }
         }
     } else {
-        measure_stdin(buffer_size, iterations);
+        measure_stdin(buffer_size, iterations, passthrough);
     }
 }
 
-fn measure_tcp_stream(address: &str, port: u16, buffer_size: usize, iterations: usize) {
+fn measure_tcp_stream(address: &str, port: u16, buffer_size: usize, iterations: usize, passthrough: bool) {
     let parsed_addr: IpAddr = match address.parse() {
         Ok(parsed) => parsed,
         Err(_) => {
             print_err!("Bad IP address {}", address);
-            std::process::exit(1);
+            exit_err();
         }
     };
 
@@ -140,13 +167,13 @@ fn measure_tcp_stream(address: &str, port: u16, buffer_size: usize, iterations: 
                 Ok((stream, incoming_addr)) => {
                     println!("Reading incoming data from {}", incoming_addr);
                     println!();
-                    measure_reader(stream, buffer_size, iterations);
+                    measure_reader(stream, buffer_size, iterations, passthrough);
                 },
 
                 Err(err) => {
                     print_err!("There was an error accepting a connection.");
                     print_err!("ERROR: {}", err);
-                    std::process::exit(1);
+                    exit_err();
                 }
             }
         },
@@ -154,19 +181,22 @@ fn measure_tcp_stream(address: &str, port: u16, buffer_size: usize, iterations: 
         Err(err) => {
             print_err!("There was an error connecting to {}", socket_addr);
             print_err!("ERROR: {}", err);
-            std::process::exit(1);
+            exit_err();
         }
     };
 }
 
-fn measure_stdin(buffer_size: usize, iterations: usize) {
+fn measure_stdin(buffer_size: usize, iterations: usize, passthrough: bool) {
     let input = stdin();
-    measure_reader(input.lock(), buffer_size, iterations);
+    measure_reader(input.lock(), buffer_size, iterations, passthrough);
 }
 
-fn measure_reader<R: Read>(mut reader: R, buffer_size: usize, iterations: usize) {
+fn measure_reader<R: Read>(mut reader: R, buffer_size: usize, iterations: usize, passthrough: bool) {
     let output = stdout();
     let mut locked_output = output.lock();
+
+    let err_out = stderr();
+    let mut locked_error = err_out.lock();
     
     let mut buffer = Vec::with_capacity(buffer_size);
     buffer.resize(buffer_size, 0);
@@ -181,10 +211,19 @@ fn measure_reader<R: Read>(mut reader: R, buffer_size: usize, iterations: usize)
                 Ok(bytes_read) => {
                     transfer_info.last_bytes_transferred += bytes_read;
                     transfer_info.total_bytes_transferred += bytes_read;
-                    if bytes_read == 0 { end_loop = true; break; }
+                    if bytes_read == 0 {
+                        end_loop = true;
+                        break;
+                    } else if passthrough {
+                        if let Err(err) = locked_output.write_all(&buffer[0..bytes_read]) {
+                            print_err_into!(locked_error, "Error while writing buffer into stdout: {}", err);
+                            exit_err();
+                        }
+                    }
                 }
+
                 Err(err) => {
-                    print_err!("Error while reading into buffer: {}", err);
+                    print_err_into!(locked_error, "Error while reading into buffer: {}", err);
                 }
             }
         }
@@ -195,7 +234,20 @@ fn measure_reader<R: Read>(mut reader: R, buffer_size: usize, iterations: usize)
             transfer_info.last_bps = bytes_per_second(transfer_info.last_bytes_transferred, duration);
             transfer_info.total_measures += 1;
             transfer_info.total_bps += transfer_info.last_bps;
-            print_info(&mut locked_output, &mut transfer_info);
+
+            let _print_result = if passthrough {
+                print_info(&mut locked_error, &mut transfer_info)
+            } else {
+                print_info(&mut locked_output, &mut transfer_info)
+            };
+
+            match _print_result {
+                Ok(_) => {},
+                Err(err) => {
+                    print_err_into!(locked_error, "Error while printing output: {}", err);
+                    exit_err();
+                }
+            }
 
             last_measured = measure_end;
             transfer_info.last_bps = 0.0;
@@ -206,25 +258,27 @@ fn measure_reader<R: Read>(mut reader: R, buffer_size: usize, iterations: usize)
     }
 }
 
-fn print_info<W: Write>(output: &mut W, transfer_info: &mut TransferInfo) {
-    if transfer_info.total_measures > 1 { term_move_up(output, 3); }
+fn print_info<W: Write>(output: &mut W, transfer_info: &mut TransferInfo) -> Result<(), std::io::Error> {
+    if transfer_info.total_measures > 1 { term_move_up(output, 3)?; }
 
     let (mem_total_transfer, unit_total_transfer) = byte_to_mem_units(transfer_info.total_bytes_transferred as f64);
     print_fixed_width(output, "Data Transferred:", 24);
     write!(output, "{:.3} {} ({} cycles)", 
-        mem_total_transfer, unit_total_transfer, transfer_info.total_measures);
-    term_clear_line(output);
+        mem_total_transfer, unit_total_transfer, transfer_info.total_measures)?;
+    term_clear_line(output)?;
 
     let (mem_single, unit_single) = byte_to_mem_units(transfer_info.last_bps);
     print_fixed_width(output, "Transfer Speed:", 24);
-    write!(output, "{:.3} {}/sec", mem_single, unit_single);
-    term_clear_line(output);
+    write!(output, "{:.3} {}/sec", mem_single, unit_single)?;
+    term_clear_line(output)?;
 
     let avg_bps = transfer_info.total_bps / transfer_info.total_measures as f64;
     let (mem_avg, unit_avg) = byte_to_mem_units(avg_bps);
     print_fixed_width(output, "Average Transfer Speed:", 24);
-    write!(output, "{:.3} {}/sec", mem_avg, unit_avg);
-    term_clear_line(output);
+    write!(output, "{:.3} {}/sec", mem_avg, unit_avg)?;
+    term_clear_line(output)?;
+
+    Ok(())
 }
 
 fn print_fixed_width<W: Write>(output: &mut W, text: &str, columns: usize) {
@@ -246,14 +300,16 @@ fn print_fixed_width<W: Write>(output: &mut W, text: &str, columns: usize) {
 
 /// Clears to the end of the current line.
 #[inline]
-fn term_clear_line<W: Write>(output: &mut W) {
-    writeln!(output, "\x1b[K");
+fn term_clear_line<W: Write>(output: &mut W) -> Result<(), std::io::Error> {
+    writeln!(output, "\x1b[K")?;
+    Ok(())
 }
 
 /// Moves the cursor up one line.
 #[inline]
-fn term_move_up<W: Write>(output: &mut W, lines: usize) {
-    write!(output, "\x1b[{}A", lines);
+fn term_move_up<W: Write>(output: &mut W, lines: usize) -> Result<(), std::io::Error> {
+    write!(output, "\x1b[{}A", lines)?;
+    Ok(())
 }
 
 fn byte_to_mem_units(bytes: f64) -> (f64, &'static str) {
